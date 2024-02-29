8a24717774d1c558e65028ca07c3480cee061d1e
eimarramos <eimarramos.sacari@gmail.com>
2024-02-29 10:47:45 +0100

    feat: Form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './modules';
 
 const routes: Routes = [
   {path: '',  redirectTo: 'home', pathMatch: 'full'},
-  {path : 'home', loadChildren: () => import('./modules').then(m => m.HomeModule)}
+  {path: 'home', loadChildren: () => import('./modules').then(m => m.HomeModule)},
+  {path: 'form', component: FormComponent}
 ];
 
 @NgModule({
