23343bb29e8f5179de01624132f640d98e716319
eimarramos <eimarramos.sacari@gmail.com>
2024-02-29 10:25:37 +0100

    feat: Adding model and data of Category

---
+++ src/app/data/category.data.ts
@@ -0,0 +1,16 @@
+import { Category } from "../models";
+
+export const Categories: Category[] = [
+    {
+        id: '1',
+        name: 'Categoria 1'
+    },
+    {
+        id: '2',
+        name: 'Categoria 2'
+    },
+    {
+        id: '3',
+        name: 'Categoria 3'
+    }   
+]
