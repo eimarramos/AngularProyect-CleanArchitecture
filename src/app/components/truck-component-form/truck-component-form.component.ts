b15bfc3278f88b5390085e6cfffbe20231ea8ba4
eimarramos <eimarramos.sacari@gmail.com>
2024-02-29 14:57:52 +0100

    feat: Categories in form

--- src/app/components/truck-component-form/truck-component-form.component.ts
+++ src/app/components/truck-component-form/truck-component-form.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { Category } from 'src/app/models';
+import { Categories } from 'src/app/data';
 
 @Component({
   selector: 'app-truck-component-form',
   standalone: true,
-  imports: [MatSelectModule, MatInputModule, MatFormFieldModule],
+  imports: [CommonModule, MatSelectModule, MatInputModule, MatFormFieldModule],
   templateUrl: './truck-component-form.component.html',
   styleUrls: ['./truck-component-form.component.scss']
 })
 export class TruckComponentFormComponent implements OnInit{
+
+  categories: Category[];
+  constructor(){
+    this.categories = Categories;
+  }
+
   ngOnInit(): void {
-    
   }
 }
